df0edf71b44b671f14477d96b0c7f000e4b7665f
TranThuc97 <tranthuc091097@gmail.com>
2023-08-07 08:20:11 +0700

    ab c

--- app/src/main/java/com/thuctran/downloadservice/MainActivity.kt
+++ app/src/main/java/com/thuctran/downloadservice/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.thuctran.downloadservice
 
+import android.Manifest
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thuctran.downloadservice.databinding.ActivityMainBinding
+import com.thuctran.downloadservice.service.DownloadService
 
 class MainActivity : AppCompatActivity() {
+    var binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding!!.btDownload.setOnClickListener{ downloadFile() }
+    }
+
+    private fun downloadFile() {
+        if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE
+            ), 101)
+            return
+        }
+
+        var intent = Intent(this,DownloadService::class.java)
+        intent.putExtra(DownloadService().KEY_LINK,binding!!.edtLink.text.toString())
+        startService(intent)
     }
 }
--- app/src/main/java/com/thuctran/downloadservice/service/DownloadService.kt
+++ app/src/main/java/com/thuctran/downloadservice/service/DownloadService.kt
@@ -2,12 +2,21 @@ package com.thuctran.downloadservice.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Environment
 import android.os.IBinder
 import android.util.Log
+import android.widget.Toast
+import com.thuctran.downloadservice.MTask
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.net.URL
+import java.net.URLConnection
 
 @Suppress("UNREACHABLE_CODE")
-class DownloadService : Service() {
-    private val KEY_LINK: String = "KEY_LINK"
+class DownloadService : Service(), MTask.OnMainCallBack {
+     val KEY_LINK: String = "KEY_LINK"
 
     companion object{
         val TAG:String = DownloadService::class.java.name
@@ -30,11 +39,41 @@ class DownloadService : Service() {
     }
 
     private fun startDownload(link: String) {
-        //
+        var mTask:MTask = MTask(KEY_LINK, this)
+        mTask.start(link)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Log.i(TAG,"onDestroy..")
+    }
+
+    override fun execTask(key: String, data: Any?, task: MTask): Any? {
+
+        //đọc ghi dữ liệu theo mảng byte
+        var link:String = data as String
+        var path: String = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)!!.path      //đường dẫn
+        path += File(link).name     //đường dẫn full
+
+        var conn:URLConnection = URL(link).openConnection()     //tạo cổng kết nối
+        var inPut:InputStream = conn.getInputStream()
+        var outPut: OutputStream = FileOutputStream(path)
+
+        var buff: ByteArray = byteArrayOf(1024.toByte())
+        var len:Int = inPut.read(buff)
+        while (len>0){
+            outPut.write(buff, 0 ,len)
+            len = inPut.read(buff)
+        }
+        outPut.close()
+        inPut.close()
+        return path
+    }
+
+
+    override fun completeTask(key: String, data: Any) {
+        Toast.makeText(this,"download done!!",Toast.LENGTH_SHORT).show()
+        Log.i("download", "link download: $data")
+        stopSelf()      //kill khi download xong
     }
 }
